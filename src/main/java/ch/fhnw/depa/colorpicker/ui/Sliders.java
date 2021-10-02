b9cd049be906da539359f392a448acdfce85d6c9
Wolfgang Weck <wolfgang.weck@surface7>
2021-10-02 19:50:30 +0200

    Wenn man die Aufteilung auf zwei Methoden weglässt, die eigentlich gar nicht nötig ist, kann man das gerade verkürzen und wieder die Instanz-Variablen eliminieren. (Prototypisch gezeigt für Sliders)

--- src/main/java/ch/fhnw/depa/colorpicker/ui/Sliders.java
+++ src/main/java/ch/fhnw/depa/colorpicker/ui/Sliders.java
@@ -7,25 +7,14 @@ import javafx.scene.layout.VBox;
 import ch.fhnw.depa.colorpicker.ColorPicker;
 
 public class Sliders {
-  private ColorPicker app;
 
   private VBox sliders;
 
-  private Slider redSlider;
-  private Slider greenSlider;
-  private Slider blueSlider;
-
   public Sliders(ColorPicker app) {
-    this.app = app;
 
-    this.redSlider = new Slider(0, 255, 0);
-    this.greenSlider = new Slider(0, 255, 0);
-    this.blueSlider = new Slider(0, 255, 0);
-
-    setDefaults();
-  }
-
-  private void setDefaults() {
+    Slider redSlider = new Slider(0, 255, 0);
+    Slider greenSlider = new Slider(0, 255, 0);
+    Slider blueSlider = new Slider(0, 255, 0);
     redSlider.valueProperty().bindBidirectional(app.getRed());
     greenSlider.valueProperty().bindBidirectional(app.getGreen());
     blueSlider.valueProperty().bindBidirectional(app.getBlue());
